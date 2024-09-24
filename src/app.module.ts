5426332de4ee52ad86b5236d972642c47c21405c
kevinfrias051@gmail.com <kevinfrias051@gmail.com>
2024-09-23 21:02:37 -0300

    FistCommit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { GenDataService } from './services/gendata.cron.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3316,
+      username: 'root',
+      password: 'cotizaempresas',
+      database: 'cotizaciones',
+      synchronize: true,
+      entities: ['dist/**/*.entity.js'],
+      logging: 'all',
+    }),ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, GenDataService],
 })
---
+++ src/empresa/entities/empresa.entity.ts
@@ -0,0 +1,48 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('empresas')
+export class Empresa {
+  @PrimaryGeneratedColumn({
+    type: 'int',
+  })
+  public id: number;
+
+  @Column({
+    name: 'codempresa',
+    length: 100,
+  })
+  public codempresa: string;
+
+  @Column({
+    name: 'empresaNombre',
+    length: 100,
+  })
+  public empresaNombre: string;
+
+  constructor(codempresa: string, empresaNombre: string) {
+    this.codempresa = codempresa;
+    this.empresaNombre = empresaNombre;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getCodempresa(): string {
+    return this.codempresa;
+  }
+
+  public setCodempresa(codempresa: string) {
+    this.codempresa = codempresa;
+  }
+
+  public getEmpresaNombre(): string {
+    return this.empresaNombre;
+  }
+
+  public setEmpresaNombre(empresaNombre: string) {
+    this.empresaNombre = empresaNombre;
+  }
+
+
+}
