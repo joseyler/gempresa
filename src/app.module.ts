f4037039895cc5538def32b9e951ecb145ca52c2
joseyler <joseyler@gmail.com>
2024-09-25 18:28:52 -0300

    config File

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import { GenDataService } from './services/gendata.cron.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, GenDataService],
 })
