055683d7f394bb40fea872ea8ade783da7a3b4be
joseyler <joseyler@gmail.com>
2024-10-07 21:18:00 -0300

    date Utils

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import DateUtils from './utils/DateUtils';
+import DateMomentUtils from './utils/DateMomentUtils';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/dates')
+  getHello(): any {
+    const fechaDesde = {
+      fecha: '2024-10-05',
+      hora: '06:00',
+    };
+    const fechaHasta = DateUtils.getRegistroFechaFromFecha(new Date());
+    const registros = DateUtils.getRegistrosEntreFechas(fechaDesde, fechaHasta);
+    return {
+      registros,
+    };
+  }
+
+  @Get('/datesTz')
+  getTZ(): any {
+    const fechaDesde = {
+      fecha: '2024-10-05',
+      hora: '19:00',
+    };
+    const fechaHasta = DateMomentUtils.getRegistroFechaFromFecha(new Date());
+    const registros = DateMomentUtils.getRegistrosEntreFechas(
+      fechaDesde,
+      fechaHasta,
+      {
+        horaDesde: '09:00',
+        horaHasta: '15:00',
+      },
+    );
+    return {
+      registros,
+    };
   }
 }
--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,3 +1,6 @@
+import { RegistroCotizacion } from 'src/model/registro.cotizacion';
+import { RegistroFecha } from 'src/model/registro.fecha';
+
 class DateUtils {
   /**
    * Toma la Fecha de Hoy y genera un objeto bajo los formatos definidos
@@ -9,9 +12,47 @@ class DateUtils {
     return '';
   }
 
-  static getRegistrosEntreFechas(fechaDesde, fechaHasta): registrosTotales;
+  static getFechaFromRegistroFecha(fecha: RegistroFecha): Date {
+    return new Date(`${fecha.fecha}T${fecha.hora}:00.000Z`);
+  }
 
-  static getRegistrosFaltantes(registrosTotales, resgistrosExistentes): registrosFaltantes;
+  // 2024-10-07T19:00:00.000Z
+  static getRegistroFechaFromFecha(fecha: Date): RegistroFecha {
+    const fechaStr = fecha.toISOString();
+    console.log(fecha);
+    console.log(fechaStr);
+    return {
+      fecha: fechaStr.substring(0, 10),
+      hora: fechaStr.substring(11, 16),
+    };
+  }
+
+  static agregarUnaHora(fecha: Date): Date {
+    const currentMils = fecha.getTime();
+    return new Date(currentMils + 1000 * 60 * 60);
+  }
+
+  static getRegistrosEntreFechas(
+    fechaDesde: RegistroFecha,
+    fechaHasta: RegistroFecha,
+  ): RegistroCotizacion[] {
+    const registros = [];
+    let fechaActual = DateUtils.getFechaFromRegistroFecha(fechaDesde);
+    const fechaLimite = DateUtils.getFechaFromRegistroFecha(fechaHasta);
+
+    while (fechaActual <= fechaLimite) {
+      registros.push(DateUtils.getRegistroFechaFromFecha(fechaActual));
+      fechaActual = DateUtils.agregarUnaHora(fechaActual);
+    }
+    return registros;
+  }
+
+  // static getRegistrosFaltantes(
+  //   registrosTotales,
+  //   resgistrosExistentes,
+  // ): RegistroCotizacion[] {
+  //   return [];
+  // }
 }
 
 // crear una interface para representar
